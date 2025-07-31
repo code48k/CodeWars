bd67f907b9797a785e0013d4644aca19b9912e0f
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-01 00:51:50 +0200

    Recibirá una matriz a y un valor x. Solo necesita comprobar si la matriz proporcionada contiene el valor.

---
+++ matrizValorX.js
@@ -0,0 +1,240 @@
+// Recibirá una matriz a y un valor x. Solo necesita comprobar si la matriz proporcionada contiene el valor.
+
+// a puede contener números o cadenas. x puede ser cualquiera de los dos.
+
+// Devuelve verdadero si la matriz contiene el valor, falso en caso contrario.
+
+function obtenerValor(){
+
+    let array = [1, 5, 8, 12, 22, 30, 33, 41, 45, 55, 78, 99];
+    let x = 5;
+
+    for (let i=0; i<array.length; i++){
+
+        if(array[i] === x){
+
+            return true
+
+        }
+
+    }
+
+    return false;
+
+}
+
+let resultado = obtenerValor();
+console.log(resultado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// function array(){
+
+//     let matriz = [1, 4, 7, 9, 15];
+//     let x = 3;
+
+//         for(let i = 0; i < matriz.length; i++){
+
+//         if (matriz[i] === x){
+
+//       return true;
+
+//         }
+
+//     }
+
+//   return false;
+// }
+
+// let resultado = array();
+// console.log(resultado); // true o false
+
+
+
