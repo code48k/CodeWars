867e0ef96d52d946c24c72600d02e5ea8f13d3c5
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-05 22:12:20 +0200

    Creé esta función para sumar cinco a cualquier número que se le pase y devolver el nuevo valor.

---
+++ sumarCinco.js
@@ -0,0 +1,12 @@
+// Creé esta función para sumar cinco a cualquier número que se le pase y devolver el nuevo valor.
+
+let num = 22;
+
+function sumar(suma){
+
+    let resultado = num + suma;
+    
+    return resultado;
+}
+
+console.log(sumar(5));
