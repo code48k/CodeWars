19dc5fed72ce971694f45005b76bfc6902a2d593
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-06 18:07:00 +0200

    La nave espacial Enterprise ha tenido un problema al crear un programa para saludar a todos los pasajeros al subir a bordo. ¡Es tu trabajo corregir el código y que el programa vuelva a funcionar!

---
+++ depurandoSaludo.js
@@ -0,0 +1,18 @@
+// Depurando la función "SayHello"
+// La nave espacial Enterprise ha tenido un problema al crear un programa para saludar a todos los pasajeros al subir a bordo. ¡Es tu trabajo corregir el código y que el programa vuelva a funcionar!
+
+// Ejemplo de salida:
+
+// Hola, Sr. Spock
+
+function saludo(){
+
+    let depurando = "SayHello";
+    depurando = "Hola, Sr. Spock";
+
+    return depurando;
+
+}
+
+let saludoCorrecto = saludo();
+console.log(saludoCorrecto);
