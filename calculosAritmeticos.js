914c0847f3077c4ee20839098af577dd0c091cdc
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-10 22:35:47 +0200

    Su tarea consiste en crear una función que realice cuatro operaciones matemáticas básicas.

---
+++ calculosAritmeticos.js
@@ -0,0 +1,32 @@
+// Su tarea consiste en crear una función que realice cuatro operaciones matemáticas básicas.
+
+// La función debe tomar tres argumentos: operación(cadena/carácter), valor1(número), valor2(número).
+// La función debe devolver el resultado de números después de aplicar la operación elegida.
+
+// Ejemplos(Operador, valor1, valor2) --> salida
+// ('+', 4, 7) --> 11
+// ('-', 15, 18) --> -3
+// ('*', 5, 5) --> 25
+// ('/', 49, 7) --> 7
+
+function calculos(operador, valor1, valor2){ // DECLARAMOS UNA FUNCIÓN CON TRES PARAMETROS '(operador, valor1, valor2'.
+
+    switch(operador){ // LA CONDICIÓN DE ESTE CONDICIONAL ES 'operador'
+        case "+":
+            return valor1 + valor2; // OPERACIÓN PARA REALIZAR LA SUMA
+        case "-":
+            return valor1 - valor2; // OPERACIÓN PARA REALIZAR LA RESTA
+        case "*":
+            return valor1 * valor2; // OPERACIÓN PARA REALIZAR LA MULTIPLICACIÓN
+        case "/":
+            return valor1 / valor2; // OPERACIÓN PARA REALIZAR LA DIVISIÓN
+        default:
+            return "Introduce un número correcto"
+    }
+    
+}
+
+console.log(calculos('+', 4, 7)); // ASIGNAMOS LOS ARGUMENTOS '(operador, valor1, valor2)', PARA LUEGO PASARLOS A LA FUNCIÓN COMO PARAMETROS, EL RESULTADO SERÍA 11.
+console.log(calculos('-', 15, 18)); // ASIGNAMOS LOS ARGUMENTOS '(operador, valor1, valor2)', PARA LUEGO PASARLOS A LA FUNCIÓN COMO PARAMETROS, EL RESULTADO SERÍA -3.
+console.log(calculos('*', 5, 5)); // ASIGNAMOS LOS ARGUMENTOS '(operador, valor1, valor2)', PARA LUEGO PASARLOS A LA FUNCIÓN COMO PARAMETROS, EL RESULTADO SERÍA 25.
+console.log(calculos('/', 49, 7)); // ASIGNAMOS LOS ARGUMENTOS '(operador, valor1, valor2)', PARA LUEGO PASARLOS A LA FUNCIÓN COMO PARAMETROS, EL RESULTADO SERÍA 7.
