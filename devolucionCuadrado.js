37ed26e3a6001fa028a0bc391b0f547adcdb1265
*** Lucas <jmlucasdelamo@gmail.com>
2025-08-30 20:33:20 +0200

    Ahora tienes que escribir una función que tome un argumento y devuelva el cuadrado del mismo.

--- devolucionCuadrado.js
+++ devolucionCuadrado.js
@@ -1,12 +1,12 @@
 // Ahora tienes que escribir una función que tome un argumento y devuelva el cuadrado del mismo.
 
-function devolucionCuadrado(numeroCuadrado){
+function devolucionCuadrado(numeroCuadrado){ // CREAMOS UNA FUNCIÓN Y LE PASAMOS UN PARAMETRO LLAMADO (numeroCuadrado).
 
-    let resultadoCuadrado = numeroCuadrado ** 2;
+    let resultadoCuadrado = numeroCuadrado ** 2; // OPERACIÓN PARA QUE DEVUELVA EL CUADRADO DEL MISMO.
 
-    return resultadoCuadrado;
+    return resultadoCuadrado; // RETORNAMOS EL VALOR DE LA VARIABLE 'resultadoCuadrado'
 }
 
-let resultado = devolucionCuadrado(5);
+let resultado = devolucionCuadrado(5); // LLAMAS A LA FUNCIÓN, Y LE PASAS COMO ARGUMENTO EL NÚMERO (5), Y MUESTRAS EL RESULTADO DE LA VARIABLE 'resultado'.
 
-console.log(resultado);
+console.log(resultado); // IMPRIMIMOS POR CONSOLA LA VARIABLE LLAMADA 'resultado' PARA MOSTRA EL RESULTADO.    
