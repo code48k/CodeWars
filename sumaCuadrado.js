59c47130ac5913fcc521774579a7bd321e7d28b8
*** Lucas <jmlucasdelamo@gmail.com>
2025-09-15 23:01:49 +0200

    Completa la función suma al cuadrado para que eleve al cuadrado cada número que se le pase y luego sume los resultados.

---
+++ sumaCuadrado.js
@@ -0,0 +1,19 @@
+// Completa la función suma al cuadrado para que eleve al cuadrado cada número que se le pase y luego sume los resultados.
+
+// Por ejemplo, para [1, 2, 2] debería devolver 9 porque 1 elevado 2 + 2 elevado a 2 +  elevado a
+
+let elevadoUno = 1 ** 2;
+let elevadoDos = 2 ** 2;
+let elevadoTres = 2 ** 2;
+
+function elevadoCuadrado(){
+
+let calculo = elevadoUno + elevadoDos + elevadoTres;
+
+return "Resultado: " + calculo;
+
+}
+
+let elevado = elevadoCuadrado();
+
+console.log(elevado);
