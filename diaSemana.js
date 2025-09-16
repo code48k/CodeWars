3bb037c0990b6ac6ef5e8f55f7b4822fe1a6d277
*** Lucas <jmlucasdelamo@gmail.com>
2025-09-16 07:41:33 +0200

    Complete la función que devuelve el día de la semana según el número ingresado:

--- diaSemana.js
+++ diaSemana.js
@@ -14,31 +14,39 @@ let valor = "Sabado";
 
 function diaSemana(){
     switch (valor){
-        
+
     case "Domingo":
         console.log("Domingo");
     break;
+
     case "Lunes":
         console.log("Lunes");
     break;
+
     case "Martes":
         console.log("Martes");
     break;
+
     case "Miercoles":
         console.log("Miercoles");
         break;
+
     case "Jueves":
         console.log("Jueves");
         break;
+
     case "Viernes":
         console.log("Viernes");
         break;
+
     case "Sabado":
         console.log("Sabado");  
         break;
+        
     default:
         console.log("En caso contrario, devuelve Incorrecto, ingrese un número entre 1 y 7");
 }
+
 }
 
 diaSemana();
