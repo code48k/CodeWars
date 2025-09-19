b0459128853f8cd4e33275f573fc0d3246cb4f66
*** Lucas <jmlucasdelamo@gmail.com>
2025-09-20 00:13:44 +0200

    En este juego, el héroe se mueve de izquierda a derecha. El jugador lanza el dado y avanza el número de espacios según el número que salga, y ese número que salga será el valor por dos veces.

--- juegoTerminal.js
+++ juegoTerminal.js
@@ -1,8 +1,10 @@
 // Función de movimiento del juego terminal.
 
-// En este juego, el héroe se mueve de izquierda a derecha. El jugador lanza el dado y avanza el número de espacios, indicado por el dado por dos veces.
+// En este juego, el héroe se mueve de izquierda a derecha. El jugador lanza el dado y avanza el número de espacios según el número que salga, y ese número que salga será el valor por dos veces.
 
 // Crea una función para el juego terminal que tome la posición actual del héroe y el resultado (1-6) y devuelva la nueva posición.
+
+// Recuerda que un dado no tiene más números (1-6).
 
 // Ejemplo:
 
@@ -10,11 +12,14 @@
 // move(2, 5) debe ser igual a 12
 // move(3, 6) debe ser igual a 15
 
-function move(pos, dados) {
-    // let resultado = dados + dados;
-    return pos + (dados) * 2;
+function move(posicion, dado){
+
+    let resultado = posicion + (dado) * 2;
+
+    return resultado;
+
 }
 
 console.log(move(0, 4));
-console.log(move(3, 6));
 console.log(move(2, 5));
+console.log(move(3, 6));
